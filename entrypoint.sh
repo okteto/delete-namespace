0666e64bf094f0dc6bc481aa6c86e9bb00aa0921
GiMo <mouhagijs@hotmail.com>
2021-07-02 09:15:16 +0200

    Added CUSTOM_CERTIFICATE
    Added the CUSTOM_CERTIFICATE environment variable for use with self-signed certificates

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,11 @@ if [ -z $namespace ]; then
   exit 1
 fi
 
+if [[ ! -z "$CUSTOM_CERTIFICATE" ]]; then
+  echo "Custom certificate is provided"
+  echo "$CUSTOM_CERTIFICATE" > /usr/local/share/ca-certificates/custom_certificate_crt
+  update-ca-certificates
+fi
+
 echo running: okteto delete namespace $namespace
 okteto delete namespace $namespace
